c634f2ebdb73a548a5b141fde42e8802e91d699b
fullgukbap <2hanjunbum6@gmail.com>
2024-09-19 15:39:38 +0900

    add: user social 정보 추가

--- internal/core/domain/domain_user.go
+++ internal/core/domain/domain_user.go
@@ -10,10 +10,19 @@ const (
 	Guest  RoleType = "GUEST"
 )
 
+type SocialType string
+
+const (
+	Google SocialType = "GOOGLE"
+	Kakao  SocialType = "KAKAO"
+	Apple  SocialType = "APPLE"
+)
+
 type User struct {
 	gorm.Model
 	Name       string
 	Resolution string
 	Role       RoleType
 	Sub        string
+	Social     string
 }
