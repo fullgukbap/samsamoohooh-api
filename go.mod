897e89a8518abdbd0307bb7ddbd567707615f34c
fullgukbap <2hanjunbum6@gmail.com>
2024-09-19 15:10:59 +0900

    feat: config 관련 로직 구현완료

--- go.mod
+++ go.mod
@@ -1,3 +1,10 @@
 module samsamoohooh-mini-api
 
 go 1.22.5
+
+require (
+	github.com/pelletier/go-toml/v2 v2.2.3
+	go.uber.org/zap v1.27.0
+)
+
+require go.uber.org/multierr v1.11.0 // indirect
---
+++ internal/infra/config/config.go
@@ -0,0 +1,44 @@
+package config
+
+import (
+	"os"
+
+	"github.com/pelletier/go-toml/v2"
+)
+
+type Config struct {
+	Database struct {
+		User            string
+		Password        string
+		Host            string
+		Port            string
+		Database        string
+		MaxIdleConns    int
+		MaxOpenConns    int
+		ConnMaxLifeTime int
+	}
+
+	Token struct {
+		Key             string
+		AccessDuration  string
+		RefreshDuration string
+	}
+}
+
+func New(path string) (*Config, error) {
+	// ? viper 사용해서 구현하는 것도 좋을듯 하다. (https://github.com/spf13/viper)
+	config := new(Config)
+
+	file, err := os.Open(path)
+	if err != nil {
+		return nil, err
+	}
+	defer file.Close()
+
+	err = toml.NewDecoder(file).Decode(config)
+	if err != nil {
+		return nil, err
+	}
+
+	return config, nil
+}
