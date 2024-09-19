78386c95e33fd69c998f353e313ebb7ec12e5ef0
fullgukbap <2hanjunbum6@gmail.com>
2024-09-19 16:28:16 +0900

    feat: GetUserByID dto 정의 완료

--- internal/core/dto/dto_user_response.go
+++ internal/core/dto/dto_user_response.go
@@ -33,3 +33,34 @@ func NewCreateUserResponse(user domain.User) (res *CreateUserResponse) {
 
 	return res
 }
+
+type GetUserByIDResponse struct {
+	ID         uint              `json:"id"`
+	Name       string            `json:"name"`
+	Resolution string            `json:"resolution"`
+	Role       domain.RoleType   `json:"role"`
+	Sub        string            `json:"sub"`
+	Social     domain.SocialType `json:"social"`
+	CreatedAt  string            `json:"createdAt"`
+	UpdatedAt  string            `json:"updatedAt"`
+	DeletedAt  string            `json:"deletedAt"`
+}
+
+func NewGetUserByIDResponse(user domain.User) (res *GetUserByIDResponse) {
+	res = &GetUserByIDResponse{
+		ID:         user.ID,
+		Name:       user.Name,
+		Resolution: user.Resolution,
+		Role:       user.Role,
+		Sub:        user.Sub,
+		Social:     user.Social,
+		CreatedAt:  user.CreatedAt.Format(CreatedAtFormat),
+		UpdatedAt:  user.UpdatedAt.Format(UpdatedAtFormat),
+	}
+
+	if user.DeletedAt.Valid {
+		res.DeletedAt = user.DeletedAt.Time.Format(DeletedAtFormat)
+	}
+
+	return res
+}
