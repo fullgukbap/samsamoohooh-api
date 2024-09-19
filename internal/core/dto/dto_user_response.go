7b71ca16546b9fb9a3306cb17c06b256f16dc0e6
fullgukbap <2hanjunbum6@gmail.com>
2024-09-19 17:30:13 +0900

    feat: user update와 관련된 dto 정의 완료

--- internal/core/dto/dto_user_response.go
+++ internal/core/dto/dto_user_response.go
@@ -64,3 +64,34 @@ func NewGetUserResponse(user domain.User) (res *GetUserResponse) {
 
 	return res
 }
+
+type UpdateUserResponse struct {
+	ID         uint              `json:"id"`
+	Name       string            `json:"name"`
+	Resolution string            `json:"resolution"`
+	Role       domain.RoleType   `json:"role"`
+	Sub        string            `json:"sub"`
+	Social     domain.SocialType `json:"social"`
+	CreatedAt  string            `json:"createdAt"`
+	UpdatedAt  string            `json:"updatedAt"`
+	DeletedAt  string            `json:"deletedAt"`
+}
+
+func NewUpdateUserResponse(user domain.User) (res *UpdateUserResponse) {
+	res = &UpdateUserResponse{
+		ID:         user.ID,
+		Name:       user.Name,
+		Resolution: user.Resolution,
+		Role:       user.Role,
+		Sub:        user.Sub,
+		Social:     user.Social,
+		CreatedAt:  user.CreatedAt.Format(CreatedAtFormat),
+		UpdatedAt:  user.UpdatedAt.Format(UpdatedAtFormat),
+	}
+
+	if user.DeletedAt.Valid {
+		res.DeletedAt = user.DeletedAt.Time.Format(DeletedAtFormat)
+	}
+
+	return res
+}
